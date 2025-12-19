9f7edbbc4ad33b8443f20fda2563bf95d78be90a
zndk-hys <hysryt+zndk@gmail.com>
2025-12-19 12:58:10 +0900

    refactor: capsule logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { useState } from "react";
 import "./App.css";
-import { compareDigits, generateRandomDigits } from "./utils/logic";
 import { Keypad } from "./components/Numpad";
 import { InputDisplay } from "./components/InputDisplay";
 import { CompareHistory } from "./components/CompareHistory";
 import { CompleteHitResult } from "./components/CompleteHitResult";
+import { useHitBlowGame } from "./hooks/useGameLogic";
 
 export type FocusIndex = number;
 export type Digits =
@@ -30,82 +29,17 @@ export type CompareHistoryRecord = {
 };
 
 function App() {
-	const [randomDigits, setRandomDigits] = useState<Digits[]>(
-		generateRandomDigits(4),
-	);
-	const [focus, setFocus] = useState<FocusIndex>(0);
-	const [inputDigits, setInputDigits] = useState<Digits[]>(Array(4).fill(""));
-	const [compareHistory, setCompareHistory] = useState<CompareHistoryRecord[]>(
-		[],
-	);
-	const [clear, setClear] = useState(false);
-
-	const onClickInputDigit = (focus: FocusIndex) => {
-		setFocus(focus);
-	};
-
-	const onClickNum = (num: Digits) => {
-		setInputDigits((inputDigits) => {
-			inputDigits[focus] = num;
-			return inputDigits.concat();
-		});
-
-		setFocus((focus) => {
-			if (focus < inputDigits.length - 1) {
-				focus++;
-			}
-			return focus;
-		});
-	};
-
-	const onClickDelete = () => {
-		if (inputDigits[focus] === "") {
-			if (focus !== 0) {
-				const leftFocus = focus - 1;
-				setFocus(leftFocus);
-				setInputDigits((inputDigits) => {
-					inputDigits[focus - 1] = "";
-					return inputDigits.concat();
-				});
-			}
-		} else {
-			setInputDigits((inputDigits) => {
-				inputDigits[focus] = "";
-				return inputDigits.concat();
-			});
-		}
-	};
-
-	const onClickSubmit = () => {
-		const compareResult = compareDigits(inputDigits, randomDigits);
-		setCompareHistory((compareHistory) => [
-			...compareHistory,
-			{
-				submitIndex: compareHistory.length + 1,
-				compareResult,
-			},
-		]);
-		resetForm();
-		if (compareResult.hit === 4) {
-			setClear(true);
-		}
-	};
-
-	const onClickReplay = () => {
-		resetGame();
-	};
-
-	const resetForm = () => {
-		setInputDigits(Array(4).fill(""));
-		setFocus(0);
-	};
-
-	const resetGame = () => {
-		resetForm();
-		setRandomDigits(generateRandomDigits(4));
-		setCompareHistory([]);
-		setClear(false);
-	};
+	const {
+		focus,
+		inputDigits,
+		compareHistory,
+		clear,
+		onClickInputDigit,
+		onClickNum,
+		onClickDelete,
+		onClickSubmit,
+		onClickReplay,
+	} = useHitBlowGame();
 
 	return (
 		<div className="box">
