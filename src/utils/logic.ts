a9fd6053c1ac8e5aafa3e092c1725d3096aec265
zndk-hys <hysryt+zndk@gmail.com>
2025-12-19 13:26:07 +0900

    refactor: random digits generator

--- src/utils/logic.ts
+++ src/utils/logic.ts
@@ -2,17 +2,13 @@ import type { CompareResult, Digits } from "../types";
 
 export function generateRandomDigits(length: number): Digits[] {
 	const values: Digits[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-	const randomDigits: Digits[] = [];
 
 	for (let i = 0; i < length; i++) {
-		const idx = Math.floor(Math.random() * (values.length - i));
-		randomDigits.push(values[idx]);
-		const tmp = values[values.length - 1 - i];
-		values[values.length - 1 - i] = values[idx];
-		values[idx] = tmp;
+		const j = Math.floor(Math.random() * (values.length - i)) + i;
+		[values[i], values[j]] = [values[j], values[i]];
 	}
 
-	return randomDigits;
+	return values.slice(0, length);
 }
 
 export function compareDigits(
